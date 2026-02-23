9afc1d3e34a5dcc362c98ac71eef277c1b6cb8d2
PRATAP K <kalepratap045@gmail.com>
2026-02-23 10:18:11 +0530

    Refactor firebase.js to use importScripts and expose auth/db

--- firebase.js
+++ firebase.js
@@ -1,18 +1,21 @@
-<!-- firebase.js -->
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-auth-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore-compat.js"></script>
+// firebase.js  (PURE JavaScript file)
 
-<script>
+importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-auth-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore-compat.js");
+
 const firebaseConfig = {
-  apiKey: "PASTE_API_KEY",
-  authDomain: "PASTE_AUTH_DOMAIN",
-  projectId: "PASTE_PROJECT_ID",
+  apiKey: "AIzaSyAvSI1FKa3icRSv93-lpL3VMRJ31hAFHgU",
+  authDomain: "bricks-websit.firebaseapp.com",
+  projectId: "bricks-websit",
+  storageBucket: "bricks-websit.firebasestorage.app",
+  messagingSenderId: "541227684303",
+  appId: "1:541227684303:web:83a89c63abe23a339c42ba",
+  measurementId: "G-E2XYKRYRRY"
 };
 
 firebase.initializeApp(firebaseConfig);
 
-const auth = firebase.auth();
-const db = firebase.firestore();
-</script>
-const firebaseConfig = { apiKey: "AIzaSyAvSI1FKa3icRSv93-lpL3VMRJ31hAFHgU", authDomain: "bricks-websit.firebaseapp.com", projectId: "bricks-websit", storageBucket: "bricks-websit.firebasestorage.app", messagingSenderId: "541227684303", appId: "1:541227684303:web:83a89c63abe23a339c42ba", measurementId: "G-E2XYKRYRRY" };
+// expose globally
+window.auth = firebase.auth();
+window.db = firebase.firestore();
